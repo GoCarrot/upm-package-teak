dd1363922cf890bce66257b1196b91ee4bb28d5f
Teak CI <team@teak.io>
2021-10-30 00:31:51 +0000

    4.1.2

--- Runtime/Plugins/Android/Version.java
+++ Runtime/Plugins/Android/Version.java
@@ -5,6 +5,6 @@ import java.util.Map;
 
 public class Version {
     public static Map<String, Object> map() {
-        return Collections.singletonMap("unity", "4.1.1");
+        return Collections.singletonMap("unity", "4.1.2");
     }
 }
